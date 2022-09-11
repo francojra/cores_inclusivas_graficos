e22eae705746e85a9cd99473f6ca4b0c6f5a8e8a
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-09-10 22:38:17 -0300

    Add files via upload

---
+++ cores_inclusivas_graficos.R
@@ -0,0 +1,22 @@
+
+# Cores Inclusivas nos Gráficos ------------------------------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 10/09/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://www.instagram.com/p/CacI4ObOfnS/ -------------------------------------------------------------------------------------
+
+# Introdução -------------------------------------------------------------------------------------------------------------------------------
+
+### O daltonismo é uma deficiência na visão de cores. Isso significa que
+### as pessoas com daltonismo podem não estar cientes das diferenças entre
+### as cores que são óbvias para as pessoas sem o daltonismo.
+
+### O pacote 'cols4all' contém várias séries de paletas de cores inclusivas
+### que são pontuadas em vários aspectos: daltonismo, presença de cores 
+### intensas que devem ser evitadas, harmonia estética geral e quantidade
+### de tons.
+
+### Em adição, para cada paleta é atribuído um valor para as cores ausentes,
+### o que é especialmente importante para visualização de dados espaciais.
+
+
+
